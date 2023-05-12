514cf809593ca7b0e9fe08bf92f1e23e2d90ddd5
R Ram Shankar <110959980+ram-shankar58@users.noreply.github.com>
2023-05-12 20:21:57 +0530

    Update q1.cpp

--- CAT-2/q1.cpp
+++ CAT-2/q1.cpp
@@ -33,3 +33,41 @@ int main(){
     arrivaltime(hours, minutes, seconds);
 
 }
+/* Another approach is as follows:
+#include <iostream>
+using namespace std;
+
+// struct to hold time values
+struct Time {
+    int hour;
+    int minute;
+    int second;
+};
+
+//Function to calculate the arrival time, takes starting time as input
+Time arrivaltime(int hours=8, int minutes=30, int seconds=30){
+    int traveltime = (10 * 3600) + (20 * 60) + 10;
+    int deptime = hours * 3600 + minutes * 60 + seconds;
+    int reachtime = traveltime + deptime;
+    int reachhour = reachtime / 3600;
+    int reachmin = (reachtime % 3600) / 60;
+    int reachsec = reachtime % 60;
+    Time reach = {reachhour, reachmin, reachsec};
+    return reach;
+}
+
+//Function to print the arrival time in 24 hour format
+void printing(Time reach){
+    printf("Arrival time is : %02d:%02d:%02d\n", reach.hour, reach.minute, reach.second);
+}
+
+int main(){
+    int hours, minutes, seconds;
+    cout<<"Enter the departure time in 24 hours format\n";
+    cout<<"Hours: Minutes: Seconds \n";
+    cin>>hours>>minutes>>seconds;
+    Time reach = arrivaltime(hours, minutes, seconds);
+    printing(reach);
+    return 0;
+}
+*/
