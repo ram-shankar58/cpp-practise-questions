2fc48c56ab8289ff5ae0c0ece5bb359f59f67f94
R Ram Shankar <110959980+ram-shankar58@users.noreply.github.com>
2023-05-12 21:47:06 +0530

    Added the question

--- CAT-2/q4.cpp
+++ CAT-2/q4.cpp
@@ -1,3 +1,12 @@
+/*Create a program to model a banking system. You haev a BankAccount class, that stores information about each acount, including the acount holder's name, acount no,
+and balance. You want to add a function to the class that allows account holders to transfer money to another account, but you need to access the private member variables of two BankAccount
+objects to do so
+
+Write a C++ program that implements a transferMoney() function as a friend function fo the BankAccount class. The transfermoney() function should take two BankAccount objects as arguments
+and transfer a specified amount of money from the first account to the second account. Make sure to include appropriate member functions in the BankAccount class to store and retrieve account
+information. Also make sure to handle case where the transfer amount exceeds the balance of the first account or where the account numbers are invalid.
+*/
+
 #include <iostream>
 using namespace std
 
