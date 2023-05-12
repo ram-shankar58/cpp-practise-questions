be62f64b6969648eabec740310c1fa2d38c143a2
Kedar Shinde <kedarshinde8011@gmail.com>
2023-05-12 21:16:28 +0530

    finished with cyclic shifting

---
+++ CAT-2/q5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class ShiftArray{
+    int arr[];
+    int shift;
+
+public:
+    ShiftArray(int inp[], int k){
+        arr = new int[inp.length()];
+
+        for(int i = 0; i < inp.length(); i++){
+            arr[i] = inp[i];
+        }
+
+        shift = k;
+    }
+
+    void cyclicShift(){
+        int temp[arr.length()];
+        int index = 0;
+        
+        for(int i = arr.length()-1; i < arr.length(); i++)
+            temp[index++] = arr[i];
+        
+        for(int i = 0; i < arr.length()-k-1; i++)
+            temp[index++] = arr[i];
+        
+        for(int i = 0; i < arr.length(); i++)
+            arr[i] = temp[i];
+    }
+};
