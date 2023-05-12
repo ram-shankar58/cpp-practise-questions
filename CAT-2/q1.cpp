7cdbcba8ee439c5ba7871cad7f3bc48178efb9f8
R Ram Shankar <110959980+ram-shankar58@users.noreply.github.com>
2023-05-12 20:19:01 +0530

    Create q1.cpp

---
+++ CAT-2/q1.cpp
@@ -0,0 +1,35 @@
+/*
+Mr Richard was travelling in a train. The train started from Chennai at 8 hours 30 minutes and 30 seconds
+It reached Madurai at 18 hrs, 50 minutes and 40 seconds, that is, it took 10 hours 20 minutes and 10 seconds
+Write a program using default arguments in C++ and calculate the arrival time with the departure being provided
+by the user. Answer should be 24:60:60 format
+*/
+
+#include <iostream>
+using namespace std;
+
+//Function to calculate the arrival time, takes starting time as input
+
+void printing(int reachtime, int reachour, int reachmin, int reachsec){
+    printf("Arrival time is : %02d:%02d:%02d\n ", reachour, reachmin, reachsec);
+
+}
+void arrivaltime(int hours=8, int minutes = 30, int seconds=30){
+    int traveltime= (10*3600)+(20*60)+(10);
+    int deptime=hours*3600+minutes*60+seconds;
+    static int reachtime=traveltime+deptime;
+    static int reachour=reachtime/3600;
+    static int reachmin=(reachtime%3600)/60;
+    static int reachsec=reachtime%60;
+    printing(reachtime, reachour, reachmin, reachsec);
+
+}
+
+int main(){
+    int hours, minutes, seconds;
+    cout<<"Enter the departure time in 24 hours format\n";
+    cout<<"Hours: Minutes: Seconds \n";
+    cin>>hours>>minutes>>seconds;
+    arrivaltime(hours, minutes, seconds);
+
+}
