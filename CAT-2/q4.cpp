1ea8998daa922190d9baf1e7a81fb6028be55610
Kedar Shinde <kedarshinde8011@gmail.com>
2023-05-12 21:02:34 +0530

    read the second page

--- CAT-2/q4.cpp
+++ CAT-2/q4.cpp
@@ -7,15 +7,48 @@ class BankAccount{
     int balance;
 
 public:
-    BankAccount(string n, int ac, int bal){
-        name = n;
-        accnumber = ac;
-        balance = bal;
-    }
+    // realised question doesn't mention constructors :P
+    // BankAccount(string n, int ac, int bal){
+    //     name = n;
+    //     accnumber = ac;
+    //     balance = bal;
+    // }
+
+    string get_name();
+    int get_accnumber();
+    int get_balance();
+
+    void set_name(string n);
+    void set_accnumber(int acc);
+    void set_balance(int bal);
 
     friend void transferMoney(BankAccount a, BankAccount b, int ammount);
 
 };
+
+string BankAccount::get_name(){
+    return name;
+}
+
+int BankAccount::get_accnumber(){
+    return accnumber;
+}
+
+int BankAccount::get_balance(){
+    return balance;
+}
+
+void BankAccount::set_name(string n){
+    name = n;
+}
+
+void BankAccount::set_accnumber(int acc){
+    accnumber = acc;
+}
+
+void BankAccount::set_balance(int bal){
+    balance = bal;
+}
 
 void transferMoney(BankAccount a, BankAccount b, int ammount) {
     if(a.balance - ammount < 0){
